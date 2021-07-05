6654bb929ba48bd551b2885bb3242c3b1f7e6cba
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2021-07-05 22:01:26 +0200

    JB: Returning bad request when contact requests fail

--- src/main/kotlin/com/example/cmsbackend/contacts/ContactController.kt
+++ src/main/kotlin/com/example/cmsbackend/contacts/ContactController.kt
@@ -1,6 +1,7 @@
 package com.example.cmsbackend.contacts
 
 import com.google.gson.Gson
+import java.net.URI
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -23,7 +25,6 @@ class ContactController(
     @GetMapping("/cms/api/contacts", produces = ["application/json"])
     @ResponseBody
     fun getContacts(): ResponseEntity<Any?> {
-
         val list = service.getContacts()
         return ResponseEntity(Gson().toJson(list), HttpStatus.OK)
     }
@@ -31,31 +32,38 @@ class ContactController(
     @GetMapping("/cms/api/contacts/{contactId}", produces = ["application/json"])
     @ResponseBody
     fun getContact(@PathVariable contactId: Int): ResponseEntity<Any?> {
-
-        val contact = service.getContact(contactId)
-        return ResponseEntity(Gson().toJson(contact), HttpStatus.OK)
+        return service.getContact(contactId).fold(
+            ifRight = { ResponseEntity(Gson().toJson(it), HttpStatus.OK) },
+            ifLeft = { ResponseEntity.badRequest().build() }
+        )
     }
 
     @PostMapping("/cms/api/contacts/new", produces = ["application/json"])
-    @ResponseStatus(HttpStatus.CREATED)
     fun saveContact(
         @RequestBody contactData: Map<String, String>
-    ) {
-        service.saveContact(contactData)
+    ): ResponseEntity<out Any> {
+        return service.saveContact(contactData).fold(
+            ifRight = { ResponseEntity.created(URI("")).build() },
+            ifLeft = { ResponseEntity.badRequest().build() }
+        )
     }
 
     @PutMapping("/cms/api/contacts/edit", produces = ["application/json"])
-    @ResponseStatus(HttpStatus.OK)
     fun updateContact(
         @RequestBody contactData: Map<String, String>
-    ) {
-        service.updateContact(contactData)
+    ): ResponseEntity<out Any> {
+        return service.updateContact(contactData).fold(
+            ifRight = { ResponseEntity.noContent().build() },
+            ifLeft = { ResponseEntity.badRequest().build() }
+        )
     }
 
     @DeleteMapping("/cms/api/contacts/{contactId}/delete", produces = ["application/json"])
-    @ResponseStatus(HttpStatus.OK)
-    fun deleteContact(@PathVariable contactId: Int) {
-        service.deleteContact(contactId)
+    fun deleteContact(@PathVariable contactId: Int): ResponseEntity<out Any> {
+       return service.deleteContact(contactId).fold(
+           ifRight = { ResponseEntity.noContent().build() },
+           ifLeft = { ResponseEntity.badRequest().build() }
+       )
     }
 
     @PostMapping("/cms/api/fileUpload", consumes = ["multipart/form-data"])
